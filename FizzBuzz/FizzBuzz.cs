2b6baa7afb3c7cc03d14390b96a0dfe967acc9d7
Elchanan Razin <etrazin@gmail.com>
2020-03-12 15:57:43 +0200

    changed file name from Class1 to FizzBuzz

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private readonly IDictionary<int, Action<int>> numberActionPairs;
+        
+    }
+}
